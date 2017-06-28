b28d7ad889e5d4d6f152fcf828775490a6f1f15e
Nima Zareian <nimazareian80@gmail.com>
2017-06-27 23:39:16 -0700

    Nima Testing

--- Josh's Code (starstruck)/PROS/1010X_8/src/arm.c
+++ Josh's Code (starstruck)/PROS/1010X_8/src/arm.c
@@ -59,8 +59,8 @@ else{
 
 void armpresets(){
   if(joystickGetDigital(1,8,JOY_UP)){arm_target_global = ARM_MAX;}
-    else if(joystickGetDigital(1,8,JOY_DOWN)){arm_target_global = ARM_GROUND;}
-    else if (joystickGetDigital(1,8,JOY_LEFT)){arm_target_global = ARM_LOW;}
-    else if(joystickGetDigital(1,8,JOY_RIGHT)){arm_target_global = ARM_PARALLEL;}
-    else{}
+  else if(joystickGetDigital(1,8,JOY_DOWN)){arm_target_global = ARM_GROUND;}
+  else if (joystickGetDigital(1,8,JOY_LEFT)){arm_target_global = ARM_LOW;}
+  else if(joystickGetDigital(1,8,JOY_RIGHT)){arm_target_global = ARM_PARALLEL;}
+  else{}
   }
---
+++ Test 3 Nima/include/ports.h
@@ -0,0 +1,21 @@
+#ifndef PORTS_H_
+#define PORTS_H_
+
+//Left Base
+#define DriveFL 2
+#define DriveMBL 1
+#define DriveBL 3
+
+//Right Base
+#define DriveBR 8
+#define DriveFR 9
+#define DriveMBR 10
+
+//Cone Lift
+#define LiftCone 4
+
+//Mobile Goal Lift and Claw
+#define LiftMG 7
+#define Claw 6
+
+#endif

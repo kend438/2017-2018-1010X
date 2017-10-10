eb85a3d8e60cddef0fd553d5aed9e8d86b790169
janxoverride <arielwang20@gmail.com>
2017-10-09 19:57:26 -0700

    fixed

--- copyandpaste/include/main.h
+++ copyandpaste/include/main.h
@@ -1,93 +1,17 @@
-/** @file main.h
- * @brief Header file for global functions
- *
- * Any experienced C or C++ programmer knows the importance of header files. For those who
- * do not, a header file allows multiple files to reference functions in other files without
- * necessarily having to see the code (and therefore causing a multiple definition). To make
- * a function in "opcontrol.c", "auto.c", "main.c", or any other C file visible to the core
- * implementation files, prototype it here.
- *
- * This file is included by default in the predefined stubs in each VEX Cortex PROS Project.
- *
- * Copyright (c) 2011-2016, Purdue University ACM SIGBots.
- * All rights reserved.
- *
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at http://mozilla.org/MPL/2.0/.
- *
- * PROS contains FreeRTOS (http://www.freertos.org) whose source code may be
- * obtained from http://sourceforge.net/projects/freertos/files/ or on request.
- */
-
 #ifndef MAIN_H_
-
-// This prevents multiple inclusion, which isn't bad for this file but is good practice
 #define MAIN_H_
-
 #include <API.h>
-
-// Allow usage of this file in C++ programs
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-// A function prototype looks exactly like its declaration, but with a semicolon instead of
-// actual code. If a function does not match a prototype, compile errors will occur.
-
-// Prototypes for initialization, operator control and autonomous
-
-/**
- * Runs the user autonomous code. This function will be started in its own task with the default
- * priority and stack size whenever the robot is enabled via the Field Management System or the
- * VEX Competition Switch in the autonomous mode. If the robot is disabled or communications is
- * lost, the autonomous task will be stopped by the kernel. Re-enabling the robot will restart
- * the task, not re-start it from where it left off.
- *
- * Code running in the autonomous task cannot access information from the VEX Joystick. However,
- * the autonomous function can be invoked from another task if a VEX Competition Switch is not
- * available, and it can access joystick information if called in this way.
- *
- * The autonomous task may exit, unlike operatorControl() which should never exit. If it does
- * so, the robot will await a switch to another mode or disable/enable cycle.
- */
 void autonomous();
-
 void initializeIO();
 Encoder encoder;
 Encoder encoder_L;
 Encoder encoder_R;
-<<<<<<< HEAD
 Encoder encoderLift;
 
 Gyro gyro;
-
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-Encoder encoderLift;
-
-
-=======
-Gyro gyro;
->>>>>>> fa57ca5fbcda22237cd8aead6fe641a2148af71e
-=======
-Gyro gyro;
->>>>>>> fa57ca5fbcda22237cd8aead6fe641a2148af71e
-/**
- * Runs user initialization code. This function will be started in its own task with the default
- * priority and stack size once when the robot is starting up. It is possible that the VEXnet
- * communication link may not be fully established at this time, so reading from the VEX
- * Joystick may fail.
- *
- * This function should initialize most sensors (gyro, encoders, ultrasonics), LCDs, global
- * variables, and IMEs.
- *
- * This function must exit relatively promptly, or the operatorControl() and autonomous() tasks
- * will not start. An autonomous mode selection menu like the pre_auton() in other environments
- * can be implemented in this task if desired.
- */
->>>>>>> 9f8e44f11a1bfd804fc125f0ccfe162a8a1ddf3c
 void initialize();
 void operatorControl();
 #ifdef __cplusplus
--- copyandpaste/src/opcontrol.c
+++ copyandpaste/src/opcontrol.c
@@ -8,10 +8,8 @@
 void operatorControl() {
 
 	float pidError;
-	float liftError;
 	int liftCurrent;
 	int pidArm;
-	int pidLift;
 	bool armRaise;
 	bool armLower;
 	bool armDown;
@@ -20,33 +18,14 @@ void operatorControl() {
 	int SensorTargetValue;
 	int SensorTargetLift;
 	int current;
-<<<<<<< HEAD
 	bool liftUp;
 	bool liftDown;
+	bool liftScore;
 
 	float pid_Kp = 1.5;//was 0.5
-	float pid_Kp_Lift = 1;
 
  SensorTargetValue = encoderGet(encoder);
  SensorTargetLift = encoderGet(encoderLift);
-=======
-<<<<<<< HEAD
-<<<<<<< HEAD
-	float pid_Kp = 0.5;
-	/*int LiftUp;
-	int LiftDown;
-	int SensorTargetValue2;
-*/
-=======
-	float pid_Kp = 1.5;//was 0.5
->>>>>>> fa57ca5fbcda22237cd8aead6fe641a2148af71e
-=======
-	float pid_Kp = 1.5;//was 0.5
->>>>>>> fa57ca5fbcda22237cd8aead6fe641a2148af71e
-
- SensorTargetValue = encoderGet(encoder);
- SensorTargetValue = encoderGet(encoderLift);
->>>>>>> 9f8e44f11a1bfd804fc125f0ccfe162a8a1ddf3c
 
 	while (1) {
 
@@ -56,70 +35,29 @@ void operatorControl() {
 		sevenCone = joystickGetDigital(2,7,JOY_UP);
 		preset = joystickGetDigital(2,7,JOY_LEFT);
 		liftUp = joystickGetDigital(1,6,JOY_UP);
-		liftDown = (joystickGetDigital(1,6,JOY_DOWN));
+		liftDown = joystickGetDigital(1,6,JOY_DOWN);
+		liftScore = joystickGetDigital(1,7,JOY_DOWN);
 
 		///********CLAW*********//
 		clawSet(joystickGetAnalog(2,2));
 
 		///***MOBILEGOAL***//
-		if(liftUp == 1 && liftDown ==0)
-		{
-			liftSet(-127);
-			SensorTargetLift = encoderGet(encoderLift);
-		}
-		else if(liftDown == 1 && liftUp == 0)
-		{
-			/*int currentLift = encoderGet(encoderLift);
-			if(currentLift > 900)
-			{
-			liftSet(0);
-		  }
-			else if(currentLift < 900)
-			{
-				*/
-			liftSet(127);
-			//}
-			SensorTargetLift = encoderGet(encoderLift);
-		}
+		if(liftUp == 1 && liftDown ==0){liftSet(-127);}
 
-		if(liftUp==0 && liftDown == 0)
-		{
-				int currentLift = encoderGet(encoderLift);
-						if(currentLift != SensorTargetLift)
-						{
-							//find encoder value now
-						liftError =  SensorTargetLift - currentLift;
-						  // calculate arm
-					  int pidLift = (pid_Kp_Lift * liftError);
+		else if(liftDown == 1 && liftUp == 0){liftSet(127);}
 
-								  // limit arm
-							 if( pidLift > 127 )
-						 		 	 pidLift = 127;
-							 if( pidLift < (-127) )
-							 		 pidLift = (-127);
-			 		 }
-			liftSet(pidLift);
+		if(liftUp==0 && liftDown == 0){liftSet(0);}
+
+		if(liftScore == 1)
+		{
+			liftCurrent = encoderGet(encoderLift);
+			if(liftCurrent > 1000){liftSet(127);}
 		}
 
 
 		///*****ENCODERSLCD*****//
-		/*int counts = encoderGet(encoder);
-		lcdPrint(uart1, 1, "encoder%d", counts);
-		lcdPrint(uart1, 2, "Target%d", SensorTargetValue);
-<<<<<<< HEAD
-*/
-		int countsLift = encoderGet(encoderLift);
-		lcdPrint(uart1, 1, "encoderLift%d", countsLift);
-=======
-<<<<<<< HEAD
-
 		int countsLift = encoderGet(encoderLift);
 		lcdPrint(uart1, 2, "encoderLift%d", countsLift);
-		delay(20);
-=======
->>>>>>> fa57ca5fbcda22237cd8aead6fe641a2148af71e
->>>>>>> 9f8e44f11a1bfd804fc125f0ccfe162a8a1ddf3c
-
 		lcdPrint(uart1, 2, "targetLift%d", SensorTargetLift);
 
 		//*****DRIVE*****//
@@ -176,115 +114,12 @@ void operatorControl() {
 					motorSet(ArmRB, 0);
 				}
 
- if(armDown == 1)
- {
-	 SensorTargetValue = 470;
-<<<<<<< HEAD
- }
+ if(armDown == 1){SensorTargetValue = 470;}
 
- if(sevenCone == 1)
- {
-	 SensorTargetValue = 245;
- }
+ if(sevenCone == 1){SensorTargetValue = 245;}
 
- if(preset == 1)
- {
- 	SensorTargetValue = 355;
- }
-delay(20);
+ if(preset == 1){SensorTargetValue = 355;}
+
+	delay(20);
 }
-
 }
-=======
-	 /*
-	int arm = encoderGet(encoder);
-	int armError =  466 - arm;
-	float armDown = (pid_Kp * armError);
-
-	// limit arm
-	if( armDown > 127 )
-		 armDown = 127;
-	if( armDown < (-127) )
-		 armDown = (-127);
-
-	motorSet(ArmT, armDown);
-	motorSet(ArmLB, armDown);
-	motorSet(ArmRB, armDown);
-*/}
-
-
- if(sevenCone != 0){
-
-	 SensorTargetValue = 245;}
-
-	 if(preset != 0){
-
-	 	SensorTargetValue = 355;}
-
-	 /*
-	int arm = encoderGet(encoder);
-	int armError =  280 - arm;
-	float armDown = (pid_Kp * armError);
-
-	// limit arm
-	if( armDown > 127 )
-		 armDown = 127;
-	if( armDown < (-127) )
-		 armDown = (-127);
-
-	motorSet(ArmT, armDown);
-	motorSet(ArmLB, armDown);
-	motorSet(ArmRB, armDown);
-*/}
-
-
-/* used to be arm without pid
-		if(joystickGetDigital(2,5,JOY_UP)){
-<<<<<<< HEAD
-			motorSet(arm,-127);
-		}
-		else if(joystickGetDigital(2,5,JOY_DOWN)){
-			motorSet(arm,127);
-		}
-		else{
-			motorSet(arm,0);
-=======
-			liftSet(-127);
-		}
-		else if(joystickGetDigital(2,5,JOY_DOWN)){
-			liftSet(127);
-		}
-		else{
-			liftSet(0);
->>>>>>> 7df428b869349c7d5171e3a44def7603c892dae8
-		}
-		*/
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-
-
-		clawSet(joystickGetAnalog(2,2));
-=======
->>>>>>> fa57ca5fbcda22237cd8aead6fe641a2148af71e
-
-		delay(20);
-	}
-<<<<<<< HEAD
-
-
-
-	/*if(SensorTargetValue2);
-
-	encoder2 = 200
-*/
-
-}
-=======
->>>>>>> fa57ca5fbcda22237cd8aead6fe641a2148af71e
-=======
-
-		delay(20);
-	}
->>>>>>> fa57ca5fbcda22237cd8aead6fe641a2148af71e
->>>>>>> 9f8e44f11a1bfd804fc125f0ccfe162a8a1ddf3c

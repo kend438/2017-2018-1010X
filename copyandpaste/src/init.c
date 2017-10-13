cb0452fdca07018abe34d9a8cae8700262a7396a
janxoverride <arielwang20@gmail.com>
2017-10-13 13:34:00 -0700

    lcd not working

--- copyandpaste/src/init.c
+++ copyandpaste/src/init.c
@@ -16,12 +16,21 @@ encoder_L = encoderInit(5, 6, 0);
 encoder_R = encoderInit(7, 8, 0);
 encoderLift = encoderInit(3, 4, 1);
 
+myauto = 1;
+
 gyro = gyroInit(1,196);
   lcdClear(uart1);
   lcdSetText(uart1, 1, "Ready");
-
+delay(20);
+  lcdClear(uart1);
+  delay(20);
 while(1){
-lcdPrint(uart1, 1, "%d", myauto);
+  lcdClear(uart1);
+lcdSetText(uart1, 2, "check");
+delay(20);
+lcdClear(uart1);
+delay(20);
+lcdPrint(uart1, 1, "myauto%d", myauto);
 if(lcdReadButtons(uart1) == 1){ // changes auto
         lcdClear(uart1);
         lcdSetText(uart1, 1, "wait");
@@ -33,4 +42,7 @@ if(lcdReadButtons(uart1) == 4){ // changes auto
         delay(100);
         myauto = myauto+1;}
       }
+      delay(20);
+      lcdSetText(uart1, 1, "extraReady");
+      delay(20);
     }
